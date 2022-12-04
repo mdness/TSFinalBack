08bb40e6aa59ccc934d99a314fac9a09df30adaf
mdness <maitecelcastelli@gmail.com>
2022-12-04 13:24:49 -0300

    Arreglos

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,39 +1,3 @@
-// import { Router, Request, Response } from 'express';
-
-// const router = Router();
-
-// router.get('/', (req: Request, res: Response) => {
-//     res.json({
-//         msg: 'Get all'
-//     })
-// })
-
-// router.post("/",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Post all"
-//     })
-// })
-
-// router.delete("/:id",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Delete all"
-//     })
-// })
-
-// router.post("/:id/products",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Add product to cart"
-//     })
-// })
-
-// router.delete("/cartID/products/:productID",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Delete product in cart"
-//     })
-// });
-
-// export default router
-
 import { Router, Request, Response } from "express";
 
 
