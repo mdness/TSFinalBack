661d9a5d2feb5504b7dad01572546c6a09ea3917
mdness <maitecelcastelli@gmail.com>
2022-12-04 13:23:23 -0300

    Arreglos

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,33 +1,3 @@
-// import { Router, Request, Response } from 'express';
-
-// const router = Router();
-
-// router.get('/:id', (req: Request, res: Response) => {
-//     res.json({
-//         msg: 'Get all'
-//     })
-// })
-
-// router.post("/",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Post all"
-//     })
-// })
-
-// router.put("/:id",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Modify product"
-//     })
-// })
-
-// router.delete("/:id",(req: Request, res: Response) => {
-//     res.json({
-//         msg: "Delete product"
-//     })
-// });
-
-// export default router
-
 import { Router, Request, Response } from "express";
 import { getAllProducts, createProduct } from '../controller/products';
 
